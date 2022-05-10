aeb7bc74fc178813c7097b548739a30878bc6cf8
Thayerres Scarpini <thayerres@gmail.com>
2022-05-10 17:37:05 -0300

    feat: Create and Remove and Complete tasks function

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!!newTaskTitle){
+      const newTasks:Task = {
+        id: tasks.length + 1,
+        title: newTaskTitle,
+        isComplete: false
+      } 
+      setTasks([...tasks,newTasks])
+      setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((tasks)=>{
+      if(tasks.id === id){
+        tasks.isComplete = !tasks.isComplete
+      }
+      return tasks
+    })
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((tasks)=>{
+      return tasks.id !== id
+    })
+    setTasks(newTasks)
   }
 
   return (
